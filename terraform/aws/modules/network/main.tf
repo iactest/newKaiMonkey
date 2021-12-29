e5087a618670e480fdac6c6916116958750b3788
iactestarjun <94528866+iactestarjun@users.noreply.github.com>
2021-12-29 16:23:08 +0530

    Update main.tf

--- terraform/aws/modules/network/main.tf
+++ terraform/aws/modules/network/main.tf
@@ -1,6 +1,7 @@
 data "aws_availability_zones" "available" {}
 
 # Create a VPC to launch our instances into
+# new check
 resource "aws_vpc" "km_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
